5c794e4335162ad07ad455d1af92eafba495ed1b
Janice <mnzd123@gmail.com>
2019-06-28 16:18:30 +0800

    使用ES6的解构赋值

--- main.js
+++ main.js
@@ -9,33 +9,42 @@ window.jQuery = function (nodeOrSelector) {
   return nodes
 }
 
-window.jQuery.ajax = function (url, method, body, success, fail) {
+window.$ = window.jQuery
+
+window.jQuery.ajax = function ({ url, method, body, successFn, failFn }) {
   let request = new XMLHttpRequest()
   request.open(method, url)
   request.onreadystatechange = () => {
     if (request.readyState === 4) {
       if (request.status >= 200 && request.status < 300) {
-        success.call(undefined, request.responseText)
+        successFn.call(undefined, request.responseText)
       } else if (request.status >= 400) {
-        fail.call(undefined, request)
+        failFn.call(undefined, request)
       }
     }
   }
   request.send(body)
 }
 
+function f1(responseText) {
+  console.log(responseText)
+}
+function f2(responseText) {
+  console.log('success')
+}
+
 mybutton.addEventListener('click', (e) => {
-  window.jQuery.ajax(
-    '/xxx',
-    'post',
-    'hello!',
-    (x) => {
-      console.log(x)
-      console.log('Success')
+  window.jQuery.ajax({
+    url: '/xxx',
+    method: 'post',
+    body: 'hello!',
+    successFn: (x) => {
+      f1.call(undefined, x)
+      f2.call(undefined, x)
     },
-    (x) => {
+    failFn: (x) => {
       console.log(x.status)
       console.log(x.responseText)
     }
-  )
+  })
 })
